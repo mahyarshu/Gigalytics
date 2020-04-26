0bc9bd021fdb64bca6bff0a4f31d87bb97e1a5fc
debosky007 <debosky007@gmail.com>
2020-04-26 20:38:51 +0100

    Revert "Update BQ3.sql"
    This reverts commit d0ad727eab53e30978f839c7660722e4a6248001.

--- scripts/BQ3.sql
+++ scripts/BQ3.sql
@@ -1,5 +1,24 @@
 -- Drop table
 
+-- DROP TABLE public."songkick_ukEvents_withName";
+
+CREATE TABLE public."songkick_ukEvents_withName" (
+	"eventId" int8 NULL,
+	"eventType" text NULL,
+	"eventUri" text NULL,
+	"ageRestriction" text NULL,
+	mbid text NULL,
+	"venueId" float8 NULL,
+	"startDate" text NULL,
+	"startTime" text NULL,
+	country text NULL,
+	"flaggedAsEnded" bool NULL,
+	"eventName" text NULL,
+	popularity float8 NULL
+);
+
+-- Drop table
+
 -- DROP TABLE public.musicbrainz_artists;
 
 CREATE TABLE public.musicbrainz_artists (
@@ -21,6 +40,7 @@ CREATE TABLE public.dim_mbartist (
 	gender float8 NULL
 );
 
+
 -- Drop table
 
 -- DROP TABLE public.dim_venues;
@@ -32,16 +52,55 @@ CREATE TABLE public.dim_venues (
 	capacity float8 NULL
 );
 
--- dw_fct_city_venue_events
+--fct_event_artist_venue
+SELECT 
+	mb.mbid as artist_id,
+	ve.venue_id,
+	ve.city,
+	count(*) count_event,
+	sum(popularity) sum_popularity,
+	avg(popularity) avg_popularity
+into fct_event_artist_venue
+FROM public."songkick_ukEvents_withName" eve --50,155
+inner join public.dim_mbartist mb on eve.mbid  = mb.mbid --48,665
+inner join public.dim_venues ve on eve."venueId"  = ve.venue_id  --46063
+group by artist_id, mb.name, ve.venue_id , city;
+
+--fct_event_venues
+SELECT 
+	ve.venue_id,
+	ve.city,
+	count(*) count_event,
+	sum(popularity) sum_popularity,
+	avg(popularity) avg_popularity
+into fct_event_venues
+FROM public."songkick_ukEvents_withName" eve --50,155
+inner join public.dim_venues ve on eve."venueId"  = ve.venue_id  --46063
+group by ve.venue_id , city;
+
+--fct_event_cities
 SELECT 
 	ve.city,
-	ve."name",
-	t."year" ,
-	t."month" ,
 	count(*) count_event,
+	sum(popularity) sum_popularity,
 	avg(popularity) avg_popularity
-into dw_fct_city_venue_events
-FROM public.songkick_uk_events eve --50,155
-inner join public.dim_venues ve on eve.venue_id  = ve.venue_id  --46063
-inner join public.dim_time t on eve.start_date = t.chart_date 
-group by city, ve."name", t."year" , t."month" ;
+into fct_event_cities
+FROM public."songkick_ukEvents_withName" eve --50,155
+inner join public.dim_venues ve on eve."venueId"  = ve.venue_id  --46063
+group by city;
+
+update fct_event_cities
+set city = 'Unknown' where city is null
+
+--fct_artist_cities
+SELECT 
+	mb.mbid as artist_id,
+	ve.city,
+	count(*) count_event,
+	sum(popularity) sum_popularity,
+	avg(popularity) avg_popularity
+into fct_artist_cities
+FROM public."songkick_ukEvents_withName" eve --50,155
+inner join public.dim_mbartist mb on eve.mbid  = mb.mbid --48,665
+inner join public.dim_venues ve on eve."venueId"  = ve.venue_id  --46063
+group by artist_id, mb.name, city;
